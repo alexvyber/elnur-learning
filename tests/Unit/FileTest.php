e0e9a3d7fbc396c9f5b66e617a12216f55b8a32d
Alexey Sokolov <alexvyber@gmail.com>
2024-06-04 15:46:05 +0300

    more tests

--- tests/Unit/FileTest.php
+++ tests/Unit/FileTest.php
@@ -22,3 +22,23 @@ test('Multiply Works', function () {
     $result = multiply(10, 2);
     expect($result)->toBe(20);
 });
+
+test('Multiply 3 numbers Works', function () {
+    $result = multiplyThree(10, 2, 2);
+    expect($result)->toBe(40);
+});
+
+test('Add 3 numbers Works', function () {
+    $result = addThree(10, 2, 2);
+    expect($result)->toBe(14);
+});
+
+test('Substract 3 numbers Works', function () {
+    $result = substractThree(10, 2, 2);
+    expect($result)->toBe(6);
+});
+
+test('Divide 3 numbers Works', function () {
+    $result = divideThree(27, 3, 3);
+    expect($result)->toBe(3);
+});
