70833da6f589397d302875e46a0fa4ec810feda1
Alexey Sokolov <alexvyber@gmail.com>
2024-06-05 17:18:22 +0300

    code clean up

--- code/File.php
+++ code/File.php
@@ -45,18 +45,6 @@ function substractThree(int $a, int $b, int $c): int
 }
 
 
-
-// 60    40     20
-//  function find_max (int $a, int $b,int $c){
-//    if ($a > $b) && ($b < $c){
-//     echo
-//    }
-//     return find_max;
-
-// }
-
-
-
 function find_max_of_two(int $a, int $b)
 {
     if ($a > $b) {
@@ -77,44 +65,39 @@ function find_min_of_two(int $a, int $b)
 }
 
 
-
-
-
-
 function avarage_of_two(int $a, int $b)
 {
     return ($a + $b) / 2;
 }
 
 
-    
+function avarage_of_three($a, $b, $c)
+{
+    return ($b + $a + $c) / 3;
+}
 
-    function avarage_of_three ($a, $b, $c) {
-        
-        return ($b + $a + $c)/3;
+
+
+function find_max($a, $b, $c)
+{
+    if ($a > $b) {
+        return $a;
+    } elseif ($b > $c) {
+        return $b;
+    } else {
+        return $c;
     }
-    
+}
 
-    
-    function find_max ($a, $b, $c){
-        if ($a > $b) {
-            return $a;
-         } elseif ($b > $c){
-            return $b;
-        } else {
-            return $c;
-        }
+
+
+function find_min($a, $b, $c)
+{
+    if ($a < $b) {
+        return $a;
+    } elseif ($b < $c) {
+        return $b;
+    } else {
+        return $c;
     }
- 
-
-    
-     function find_min ($a, $b, $c){
-        if ($a <$b){
-            return $a;
-        } elseif ($b < $c){
-            return $b;
-        } else {
-            return $c;
-        }
-     }
-     
+}
